0b24de8a305d306603fae6313e964c5699128bce
aditya <144452471+Anandrajoria@users.noreply.github.com>
2024-01-22 12:43:52 +0530

    Add files via upload

---
+++ array01.js
@@ -0,0 +1,34 @@
+//array
+
+// let arr=[1,2,6,true,'aditya','krishna']
+// console.log(arr[0])
+
+//array operation
+// arr.push(23)
+// arr.push(868)
+// arr.pop()
+
+// arr.unshift(65)
+// arr.shift()
+
+
+// console.log(arr.includes(2))
+// console.log(arr.indexOf(2))
+// console.log(arr.indexOf(234))
+
+// let newarr=arr.join()
+// console.log(arr)
+// console.log(typeof newarr)
+// console.log( newarr)
+
+//slice,spice
+
+let arr=[1,2,3,4,5,6,7,8,9]
+let slice=arr.slice(1,3);
+console.log(arr)
+console.log('slice',slice)
+console.log(arr)
+
+let splice=arr.splice(1,3);
+console.log('spice',splice)
+console.log(arr)
---
+++ array02.js
@@ -0,0 +1,20 @@
+// let actress=['black widow',[1,2,3,4],'wondar women',['ironman','caption america',['adi','anand','krishna'],'thor'],'wanda']
+
+// let real=actress.flat(Infinity);
+// // console.log(real)
+
+
+// console.log(Array.isArray('aditya'))
+// console.log(Array.from('aditya'))
+// console.log(Array.from({name:'aditya'}))
+
+// let score=100
+// let score2=200
+// let score3=300
+// console.log(Array.of(score,score2,score3))
+
+
+let a=[1,2,3,4,5,6]
+for(let no of a){
+    console.log(no)
+}
